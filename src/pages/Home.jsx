c34fed079cd5ac1c68cedb2a91de668d4995da14
Jerome-Chauncey <jerome.chauncey@student.moringaschool.com>
2025-04-21 16:19:50 +0300

    Adds:Disclaimer Component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from "react";  
+import Disclaimer from "../components/Disclaimer";
 
 function Home(){
     return(
         <div>
-
+            <Disclaimer/>
         </div>
     )
 }
