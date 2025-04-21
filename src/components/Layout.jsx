b7c4a9f8784c174bccffd16cfd0f5c71b347ccfb
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-04-21 17:53:01 +0300

    Layouts

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
+import Disclaimer from "./Disclaimer";
 
 const Layout = () => {
   return (
     <>
+      <Disclaimer/>
       <Navbar />
       <main>
         <Outlet />
