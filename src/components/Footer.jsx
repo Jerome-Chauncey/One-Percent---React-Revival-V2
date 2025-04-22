9f37b8db225c1c69a526531209bbb7a091f755e0
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-04-22 10:05:26 +0300

    changes the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -147,33 +147,7 @@ const Footer = () => {
         </div>
 
         <br />
-
-        <div className="d-flex justify-content-center">
-          <div className="text-start">
-            <p>
-              Margined FX and contracts for difference are complex leveraged
-              products which carry a high level of risk and can result in losses
-              that exceed your initial investment. We recommend you seek
-              professional advice before investing.
-            </p>
-            <p>
-              Any analysis, opinion, commentary or research-based material on
-              our website is for information and educational purposes only and
-              is not, under any circumstances, intended to be an offer,
-              recommendation, advice or solicitation to buy or sell.
-            </p>
-            <p>
-              The information on this site is not directed at residents of the
-              United States, Belgium, Canada and Singapore and is not intended
-              for use by any person in any jurisdiction where such use would be
-              contrary to local law or regulation.
-            </p>
-            <div className="copyright px-2 px-sm-0">
-              <p>© 2025 One Percent, All Rights Reserved</p>
             </div>
-          </div>
-        </div>
-      </div>
     </footer>
   );
 };
