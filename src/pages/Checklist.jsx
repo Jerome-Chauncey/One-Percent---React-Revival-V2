d9b72555471a1e335b97542d83fa531d44c05ade
Jerome-Chauncey <jerome.chauncey@student.moringaschool.com>
2025-04-23 02:44:41 +0300

    creates:ResponseTable component. imports:ResponseTable to Checklist.jsx. Adds:useState to Checklist.jsx. Adds: normalized variable to Checklist.jsx. Edits:BrokerForm.jsx. Formats:Checklist.jsx. Fixes:className in ButtonDisclaimer

--- src/pages/Checklist.jsx
+++ src/pages/Checklist.jsx
@@ -1,15 +1,40 @@
 // src/pages/Checklist.jsx
-import React from "react";
+import React, { useState } from "react";
 import BrokerForm from "../components/BrokerForm";
+import ResponseTable from "../components/ResponseTable";
 
 const Checklist = () => {
+  const [responses, setResponses] = useState([]);
   const handleFormSubmit = (formData) => {
-    
+    const normalized = {
+      brokerName: formData.brokerName, 
+      tradingConditionsTransparent: formData.tradingConditions,
+      regulatoryStatusVerified: formData.regulatoryStatus,
+      reviewsRead: formData.traderReviews,
+      risksUnderstood: formData.understandRisks,
+      realisticReturns: formData.realisticReturns,
+      withdrawalPoliciesChecked: formData.withdrawalPolicies,
+      customerSupportVerified: formData.customerSupport,
+      hiddenFees: formData.hiddenFees,
+      riskManagementTools: formData.riskManagement,
+    };
+
     console.log("Form submitted:", formData);
-  }
+    setResponses((prevResponses) => [...prevResponses, normalized]);
+  };
   return (
-    <div>
-      <BrokerForm onSubmit={handleFormSubmit}/>
+    <div className="container-fluid my-5 ">
+      <div
+          className="row"
+      >
+        <div className="col-12 col-md-4 mb-4">
+          <BrokerForm onSubmit={handleFormSubmit} />
+        </div>
+
+        <div className="col-12 col-md-8"> 
+          <ResponseTable responses={responses} />
+        </div>
+      </div>
     </div>
   );
 };
